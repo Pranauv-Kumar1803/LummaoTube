3ede2562f9b2982fa6eea39252952b808ba64c67
unknown <pranauv1803@gmail.com>
2023-05-31 12:49:17 +0530

    minor changes 6

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {origin: ["https://lummaotube.onrender.com"]}
+const corsOptions = {origin: ["https://lummaotube.onrender.com"], credentials: true}
 app.use(cors(corsOptions))
 
 // routes
