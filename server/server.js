f643cd317d621674736387861014caa15623b302
unknown <pranauv1803@gmail.com>
2023-05-31 12:48:28 +0530

    minor changes 6

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {origin: ["https://lummaotube.onrender.com"]}
-app.use(cors())
+app.use(cors(corsOptions))
 
 // routes
 app.use('/api/auth',authRoutes);
